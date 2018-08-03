3c1ba6233f9ae579e9992f2e9022c1e8fcde2979
themadpillow <mouhanpatumakura@gmail.com>
2018-08-03 18:52:13 +0900

    全員に配布する処理忘れ

--- src/Commands.java
+++ src/Commands.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
 	public Main Main;
@@ -26,6 +27,11 @@ public class Commands implements CommandExecutor {
 			if(args.length != 0){
 				if(Bukkit.getPlayerExact(args[0]) != null){
 					Main.giveMap(Bukkit.getPlayerExact(args[0]));
+				}
+			}
+			else{
+				for(Player p : Bukkit.getOnlinePlayers()){
+					Main.giveMap(p);
 				}
 			}
 		}
